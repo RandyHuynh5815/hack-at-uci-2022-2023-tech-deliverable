ff8bcbbdcd760fbc5ed2e2509e22e67ba120e27e
Randy Huynh <randyhuynh5815@gmail.com>
2022-03-23 14:43:20 -0700

    Setup completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Form } from "react-bootstrap";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <Form></Form>
+      <img src="petr.png"/>
+    </div>
+  );
+}
+
+export default App;
