085e25d436b11afb7ec31cc8d91261d0ce89b2f4
Randy Huynh <randyhuynh5815@gmail.com>
2022-03-24 14:52:47 -0700

    Added basic form validation

--- src/components/application/Application.jsx
+++ src/components/application/Application.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Form, Button, Feedback } from 'react-bootstrap';
 import "./application.scss";
 
 export default function Application() {
+  const [validated, setValidated] = useState(false)
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <div className='application'>
-      <Form id="form">
+      <Form noValidate validated={validated} onSubmit={handleSubmit} id="form">
         <h1>Hack UCI Application</h1>
-        <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Group hasValidation className="mb-3" controlId="formBasicName">
           <Form.Label className='formLabel'>Name</Form.Label>
-          <Form.Control className="formControl" type="name" placeholder="Name" />
+          <Form.Control className="formControl" type="name" placeholder="Name" required/>
+          <Form.Control.Feedback type="invalid">Please enter a name</Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group hasValidation className="mb-3" controlId="formBasicEmail">
           <Form.Label className='formLabel'>Email</Form.Label>
-          <Form.Control className="formControl" type="email" placeholder="Email" />
+          <Form.Control className="formControl" type="email" placeholder="Email" required/>
+          <Form.Control.Feedback type="invalid">Please enter an email</Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicFunFact">
+        <Form.Group hasValidation className="mb-3" controlId="formBasicFunFact">
           <Form.Label className='formLabel'>Fun Fact</Form.Label>
-          <Form.Control className="formControl" as="textarea" rows={5} placeholder="Fun Fact"/>
+          <Form.Control className="formControl" as="textarea" rows={5} placeholder="Fun Fact" required/>
+          <Form.Control.Feedback type="invalid">Please enter a fun fact</Form.Control.Feedback>
         </Form.Group>
 
         <Button variant="primary" type="submit" className='button'>
